eed00977ed9eb4272fcb9df34d2abb8dadfa9276
Santi Knight <santiknight2003@gmail.com>
2022-05-05 10:30:21 -0300

    Cambio en javascript para validacion de texto

--- app.js
+++ app.js
@@ -5,12 +5,36 @@ let btnEncriptar = document.querySelector(".boton-encriptar");
 let btnDesencriptar = document.querySelector(".boton-desencriptar");
 let btnCopiarTexto = document.querySelector(".btn-copiar");
 
+let letras_incorrectas="ABCDEFGHYJKLMNÑOPQRSTUVWXYZáéíóú";
+
+function tiene_letras_incorrectas(texto){
+   for(i=0; i<texto.length; i++){
+      if (letras_incorrectas.indexOf(texto.charAt(i),0)!=-1){
+        return true;
+      }
+   }
+   return false;
+}
+
+let numeros="0123456789";
+
+function tiene_numeros(texto){
+   for(i=0; i<texto.length; i++){
+      if (numeros.indexOf(texto.charAt(i),0)!=-1){
+         return 1;
+      }
+   }
+   return 0;
+}
+
 btnEncriptar.addEventListener("click", function(e){
     e.preventDefault();
     
     let texto = encriptarTexto();
     salida.value = texto;
-    btnCopiarTexto.style.display = "block";
+    if(texto!==""){
+        btnCopiarTexto.style.display = "block";
+    }
 });
 
 btnDesencriptar.addEventListener("click", function() {
@@ -21,45 +45,57 @@ btnDesencriptar.addEventListener("click", function() {
 });
 
 btnCopiarTexto.addEventListener("click", function(e){
-
     copiarTexto();
+    salida.value = "";
 });
 
 function encriptarTexto() {
-    let textoEncriptar = limpiarTexto(mensaje.value);
-    let textoEncriptado = mensaje.value;
+    if(tiene_letras_incorrectas(mensaje.value) || tiene_numeros(mensaje.value)){
+        alert("Ingrese texto válido, sin mayúsculas, tíldes ni numeros");
+        return "";
+    }
+    else{
+        let textoEncriptar = limpiarTexto(mensaje.value);
+        let textoEncriptado = mensaje.value;
 
-    textoEncriptado = textoEncriptar.replaceAll("e", "enter")
-        .replaceAll("i", "imes")
-        .replaceAll("a", "ai")
-        .replaceAll("o", "ober")
-        .replaceAll("u", "ufat")
-    
-    mensaje.value = "";
-    return textoEncriptado;
+        textoEncriptado = textoEncriptar.replaceAll("e", "enter")
+            .replaceAll("i", "imes")
+            .replaceAll("a", "ai")
+            .replaceAll("o", "ober")
+            .replaceAll("u", "ufat")
+        
+        mensaje.value = "";
+        return textoEncriptado;
+    }
 }
 
 function desencriptarTexto() {
-    let textoEncriptar = limpiarTexto(mensaje.value);
-    let textoDesencriptado = mensaje.value;
+    if(tiene_letras_incorrectas(mensaje.value) || tiene_numeros(mensaje.value)){
+        alert("Ingrese texto válido, sin mayúsculas, tíldes ni numeros");
+        return "";
+    }
+    else{
+        let textoEncriptar = limpiarTexto(mensaje.value);
+        let textoDesencriptado = mensaje.value;
 
-    textoDesencriptado = textoEncriptar.replaceAll("enter", "e")
-        .replaceAll("imes", "i")
-        .replaceAll("ai", "a")
-        .replaceAll("ober", "o")
-        .replaceAll("ufat", "u")
+        textoDesencriptado = textoEncriptar.replaceAll("enter", "e")
+            .replaceAll("imes", "i")
+            .replaceAll("ai", "a")
+            .replaceAll("ober", "o")
+            .replaceAll("ufat", "u")
 
-    mensaje.value="";
-    return textoDesencriptado;
+        mensaje.value="";
+        return textoDesencriptado;
+    } 
 }
 
 function copiarTexto() {
     salida.focus();
     document.execCommand('selectAll');
     document.execCommand('copy');
-    btnCopiarTexto.style.display = "none"
+    btnCopiarTexto.style.display = "none";
 
-    alert("texto copiado!");
+    alert("Texto copiado!");
 }
 
 function limpiarTexto(texto){
